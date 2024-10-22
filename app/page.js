6dab2d82bb256beeab66d42e5003aa140877af16
Shailendra <shailendraajsingh@gmail.com>
2024-10-22 08:39:35 +0530

    Conf Pwa manually

--- app/page.js
+++ app/page.js
@@ -5,8 +5,11 @@ import { useEffect } from "react";
 export default function Home() {
 
   useEffect(() => {
+    console.log('sw.js register fu')
     if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+      console.log('sw.js register fu 2')
       window.addEventListener('load', () => {
+      console.log('sw.js register fu 3 after load')
         navigator.serviceWorker.register('/sw.js')
           .then(registration => {
             console.log('Service Worker registered with scope: ', registration.scope);
--- next.config.mjs
+++ next.config.mjs
@@ -1,62 +1,6 @@
-import withPWA from 'next-pwa';
-
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
     reactStrictMode: false,
-    swcMinify: true,            // Enable SWC minification for improved performance
-    compiler: {
-        removeConsole: process.env.NODE_ENV !== "development"     // Remove console.log in production
-    }
 };
 
-// export default nextConfig;
-
-export default withPWA({
-    dest: "public",         // destination directory for the PWA files
-    runtimeCaching: [
-        {
-            urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp|css|js|woff2?|eot|ttf|otf)$/,
-            handler: 'CacheFirst',
-            options: {
-                cacheName: 'static-assets',
-                expiration: {
-                    maxEntries: 100,
-                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-                },
-            },
-        },
-        {
-            urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/,
-            handler: 'CacheFirst',
-            options: {
-                cacheName: 'google-fonts',
-                expiration: {
-                    maxEntries: 30,
-                    maxAgeSeconds: 365 * 24 * 60 * 60, // 1 year
-                },
-            },
-        },
-        {
-            urlPattern: /\/.*\.(?:html|htm)$/,
-            handler: 'NetworkFirst',
-            options: {
-                cacheName: 'html-cache',
-                expiration: {
-                    maxEntries: 20,
-                    maxAgeSeconds: 7 * 24 * 60 * 60, // 1 week
-                },
-            },
-        }
-    ],
-    fallbacks: {
-        //image: "/static/images/fallback.png",
-        document: "/offline", // if you want to fallback to a custom page rather than /_offline
-        // font: '/static/font/fallback.woff2',
-        // audio: ...,
-        // video: ...,
-    },
-    disable: process.env.NODE_ENV === "development",        // disable PWA in the development environment
-    register: true,         // register the PWA service worker
-    skipWaiting: true,      // skip waiting for service worker activation
-})(nextConfig);
+export default nextConfig;
