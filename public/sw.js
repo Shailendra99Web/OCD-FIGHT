64cc3c86e7a0366919d021f56317641e06012f49
Shailendra <shailendraajsingh@gmail.com>
2024-10-22 08:47:08 +0530

    sending sw.js file

---
+++ public/sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'ocd-fight-cache-v1';
+const urlsToCache = [
+    '/',
+    '/manifest.json',
+    '/images/ocdLogo-192x192.png',
+    '/images/ocdLogo.png',
+    '/about',
+    '/history',
+    '/reset',
+    '/setIntervals',
+];
+
+// Install the service worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch requests
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Activate the service worker and remove old caches
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
